a36e3886df153e89d869f3d96c3a567096e22b6d
Sarah Meyer <Smeyer@Sarahs-MacBook-Pro.local>
2017-09-28 10:22:01 -0600

    adding styling

--- src/components/Story.js
+++ src/components/Story.js
@@ -6,7 +6,7 @@ export default class Story extends Component {
   render() {
     return (
       <div className="story" id="Story">
-      <img src="https://png.pngtree.com/element_pic/00/16/10/1157fcf731c3f7b.jpg" alt="No Image Available"/>
+      <img className="lavender" src="https://cdn.monq.com/wp-content/uploads/2016/05/Lavender.png" alt="No Image Available"/>
       <h1>Our Story</h1>
         <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer interdum tincidunt risus, vel iaculis orci sollicitudin eu. Aliquam mollis, nunc nec mattis ultrices, arcu mauris pulvinar justo, a tincidunt risus mi eget ante. Vivamus ut sagittis magna. Integer gravida pellentesque ultrices. Nunc consequat ut quam eget dignissim. Phasellus pellentesque tincidunt quam efficitur suscipit. Mauris quis sem nec magna auctor fermentum nec nec justo. Vivamus nunc risus, commodo eget sem sit amet, faucibus vulputate mauris. Maecenas sagittis, urna eget iaculis finibus, mauris nulla consectetur sem, ac sagittis lacus ipsum at orci. Suspendisse non consequat neque. In fermentum a sem eget ultricies. Duis vehicula sem ut nulla porttitor, at facilisis libero varius. Nam ut eros dolor. Curabitur tempor purus vitae nunc tempus, at ullamcorper velit viverra. Integer sapien ligula, dictum sit amet est porta, pretium malesuada elit. Nullam venenatis molestie vehicula.
